60120edebd49472e437e0218a7334699f56b6878
larryaubstore <laurence.morin-daoust@gmail.com>
2018-12-15 19:47:54 -0500

    Ajout d3 éditeur.

--- csound-nodeeditor-webpack/src/main.js
+++ csound-nodeeditor-webpack/src/main.js
@@ -1,11 +1,17 @@
 /* eslint-disable indent, quotes, no-unused-vars, eqeqeq */
 import Vue                from 'vue';
-import NodeEditor         from './components/NodeEditor.vue';
+// import NodeEditor         from './components/NodeEditor.vue';
+import Linen         from './components/Linen.vue';
 
 Vue.config.productionTip = false
 
 var instance = new Vue({
-  render: h => h(NodeEditor),
+  render: function (createElement) {
+    return createElement('div',
+        Array.apply(null, { length: 2 }).map(function () {
+            return createElement(Linen)
+        }));
+  },
   mounted: function () {
   }
 });
