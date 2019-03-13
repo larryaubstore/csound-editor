420be555a2788f11ec85627c5f4e9d6cf3e84a49
larryaubstore <laurence.morin-daoust@gmail.com>
2019-03-12 21:42:25 -0400

    Ajout du composant 'linen'.

---
+++ csound-nodeeditor-webpack/src/components/d3Component/linen.js
@@ -0,0 +1,50 @@
+/* eslint-disable indent */
+import * as d3      from 'd3';
+
+export class Linen {
+    draw(g) {
+        var halfcircle = function(x, y, rad) {
+            var arc = d3.arc();
+            return g.append('path')
+            // .attr('transform', 'translate('+[x,y]+')')
+            .attr('transform', 'rotate(180)')
+            .attr('style', 'fill:none;stroke:black;stroke-width:3')
+            .attr('d', arc({
+                innerRadius: 0,
+                outerRadius: rad,
+                startAngle: -Math.PI * 0.5,
+                endAngle: Math.PI * 0.5
+            }));
+        };
+        g.append('svg:line')
+            .attr('x1', '0')
+            .attr('y1', '50')
+            .attr('x2', '0')
+            .attr('y2', '60')
+            .attr('style', 'stroke:black;stroke-width:3');
+
+        g.append('svg:line')
+            .attr('x1', '-30')
+            .attr('y1', '0')
+            .attr('x2', '-30')
+            .attr('y2', '-10')
+            .attr('style', 'stroke:black;stroke-width:3');
+        g.append('svg:text')
+          .attr('x', -45)
+          .attr('y', -15)
+          .text((d) => 'amp');
+
+        g.append('svg:line')
+            .attr('x1', '30')
+            .attr('y1', '0')
+            .attr('x2', '30')
+            .attr('y2', '-10')
+            .attr('style', 'stroke:black;stroke-width:3');
+        g.append('svg:text')
+          .attr('x', 20)
+          .attr('y', -15)
+          .text((d) => 'freq');
+
+        halfcircle(0, 0, 50).style('opacity', 1.0)
+    }
+}
